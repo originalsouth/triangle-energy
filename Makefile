3440e02d642da47f222270b137ddf94cd3f2b20f
Benny van Zuiden <zuiden@lorentz.leidenuniv.nl>
2016-10-05 11:06:49 +0200

    Remove main loop

--- Makefile
+++ Makefile
@@ -1,6 +1,14 @@
 CC=g++
 triang: triang.cc
-	$(CC) -Wall -Wextra -std=c++11 -O3 -flto -o triang triang.cc
+	swig -c++ -python triang.i
+	$(CC) -Wall -Wextra -O3 -fPIC -flto -c triang.cc
+	$(CC) -Wall -Wextra -O3 -fPIC -flto -c triang_wrap.cxx -I/usr/include/python2.7
+	$(CC) -shared triang.o triang_wrap.o -o _triang.so
 
 clean:
-	rm -f triang
+	rm -f triang.o
+	rm -f triang_wrap.cxx
+	rm -f triang_wrap.o
+	rm -f _triang.so
+	rm -f triang.py
+	rm -f *.pyc
--- triang.cc
+++ triang.cc
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-double del=0.0725;
+double delta=0.0725;
 double a=0.1;
 double b=0.01;
 double k=10.0;
@@ -14,13 +14,14 @@ double d(double x1,double y1,double x2,double y2)
 
 double VY(double r)
 {
-    return b/(r*exp(k*r));
+    return b/r;
+    //return b/(r*exp(k*r));
 }
 
 double V3(double th0,double th1,double th2)
 {
-    const double x[6]={-del*cos(th0),del*cos(th0),a-del*cos(th1),a+del*cos(th1),a/2.0-del*cos(th2),a/2.0+del*cos(th2)};
-    const double y[6]={-del*sin(th0),del*sin(th0),-del*sin(th1),del*sin(th1),sqrt(3.0)*a/2.0-del*sin(th2),sqrt(3.0)*a/2.0+del*sin(th2)};
+    const double x[6]={-delta*cos(th0),delta*cos(th0),a-delta*cos(th1),a+delta*cos(th1),a/2.0-delta*cos(th2),a/2.0+delta*cos(th2)};
+    const double y[6]={-delta*sin(th0),delta*sin(th0),-delta*sin(th1),delta*sin(th1),sqrt(3.0)*a/2.0-delta*sin(th2),sqrt(3.0)*a/2.0+delta*sin(th2)};
     double V=0.0;
     V+=VY(d(x[0],y[0],x[2],y[2]));
     V+=VY(d(x[0],y[0],x[3],y[3]));
@@ -35,18 +36,4 @@ double V3(double th0,double th1,double th2)
     V+=VY(d(x[3],y[3],x[4],y[4]));
     V+=VY(d(x[3],y[3],x[5],y[5]));
     return V;
-}
-
-int main()
-{
-    const double h=1e3;
-    const double pi=2.0*asin(1.0);
-    double eth0=0.0,eth1=0.0,eth2=0.0,V,Vmin=1e99;
-    for(double th0=0.0;th0<=pi;th0+=pi/h) for(double th1=0.0;th1<=pi;th1+=pi/h) for(double th2=0.0;th2<=pi;th2+=pi/h)
-    {
-        V=V3(th0,th1,th2);
-        if(V<Vmin) Vmin=V,eth0=th0,eth1=th1,eth2=th2;
-    }
-    printf("(%F,%F,%F) --> %F\n",eth0,eth1,eth2,V);
-    return EXIT_SUCCESS;
 }
